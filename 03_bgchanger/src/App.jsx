7d8a9efe7195b67508ccc96b01d7d8bf76211bf3
PrasannaHadke <prasanna.hadke05@gmail.com>
2025-09-06 15:11:56 +0530

    added bgchanger

---
+++ 03_bgchanger/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import BgChanger from './Components/BgChanger'
+
+function App() {
+
+  return (
+    <>
+      <BgChanger />
+    </>
+  )
+}
+
+export default App
